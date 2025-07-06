d0779c32ced7eb5601bc36bb385ac8b3158bc311
Samarth Vohra <officialdevelopersamarth@gmail.com>
2025-07-06 20:54:15 +0530

    day 1 project codes

--- Lecture-10/swiggy_day_01/src/App.jsx
+++ Lecture-10/swiggy_day_01/src/App.jsx
@@ -1,3 +1,4 @@
+import ApiCalling from "./components/ApiCalling"
 import Body from "./components/Body"
 import Footer from "./components/Footer"
 import Header from "./components/Header"
--- Lecture-10/swiggy_day_01/src/components/Body.jsx
+++ Lecture-10/swiggy_day_01/src/components/Body.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import RestaurantCard from './RestaurantCard'
+import ApiCalling from './ApiCalling'
 
 function Body() {
+
+    let restArr = ApiCalling()
+
   return (
     <div>
         <h1 className='font-bold text-2xl m-12'>Restaurants with online food delivery in Chhindwara</h1>
@@ -9,14 +13,13 @@ function Body() {
         <button className='border rounded-xl ml-16 w-1/12 p-2'>Reset</button>
 
         <div className='flex flex-wrap m-auto w-10/12'>
-            <RestaurantCard />
-            <RestaurantCard />
-            <RestaurantCard />
-            <RestaurantCard />
-            <RestaurantCard />
-            <RestaurantCard />
-            <RestaurantCard />
-            <RestaurantCard />
+            {
+                restArr.map((item)=>{
+                    return(
+                        <RestaurantCard item={item} />
+                    )
+                })
+            }
         </div>
     </div>
   )
